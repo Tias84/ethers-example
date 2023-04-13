aee2aa2a8ca2465500831d02f71eecd5b506db82
BigTias <matias.torres@knobs.it>
2023-04-13 20:00:00 +0200

    Added ethers.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ethers } from "ethers";
+import { useState } from "react";
+
+declare global {
+  interface Window {
+    ethereum: ethers.Eip1193Provider;
+  }
+}
+const provider = new ethers.BrowserProvider(window.ethereum);
 
 function App() {
+  const [signer, setSigner] = useState<ethers.JsonRpcSigner>();
+  const [address, setAddress] = useState("");
+  const [balance, setBalance] = useState("");
+  const [beneficiary, setBeneficiary] = useState("");
+  const [amount, setAmount] = useState("");
+
+  const connectWallet = async () => {
+    const response = await provider.getSigner();
+
+    setSigner(response);
+    setAddress(response.address);
+  };
+
+  const getBalance = async () => {
+    const respone = await provider.getBalance(address);
+
+    setBalance(respone.toString());
+  };
+
+  const sendTransaction = async () => {
+    if (signer) {
+      const txn = await signer.sendTransaction({
+        to: beneficiary,
+        value: amount,
+      });
+
+      await txn.wait();
+      console.log(txn);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={connectWallet}>Collega address</button>
+      <button onClick={getBalance}>Balance</button>
+      <p>Address: {address}</p>
+      {balance ? (
+        <p>Balance: {ethers.formatEther(balance)}</p>
+      ) : (
+        "Clicca su Balance"
+      )}
+
+      <div>
+        <h1>Send transaction</h1>
+        <input
+          type="text"
+          placeholder="To address"
+          name="beneficiary"
+          value={beneficiary}
+          onChange={(e) => setBeneficiary(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Amount"
+          name="amount"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <button onClick={sendTransaction}>Send</button>
+      </div>
     </div>
   );
 }
